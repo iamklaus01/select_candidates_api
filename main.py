cbed84607f09eeb3cc6f15450e41136a3a97f565
iamklaus <iamklaus01@gmail.com>
2022-07-25 16:36:37 +0100

    Feat:Connect to database

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from .database import database
+
+
+app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
