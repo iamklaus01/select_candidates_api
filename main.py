6bacff9c1638f42e1cbc775a4f01eab680d356d5
iamklaus <iamklaus01@gmail.com>
2022-07-28 16:50:56 +0100

    Feat: Event on app for connecting to database, Routes integration

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from .database import database
+from database import database
+from routers import auth, candidatesFile, selectionFile, feature, intConstraint, enumConstraint
 
-
-app = FastAPI()
+app = FastAPI(title="Celect")
 
 
 @app.on_event("startup")
@@ -12,3 +12,10 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
+
+app.include_router(auth.router,prefix="/admin", tags=["Administration"])
+app.include_router(candidatesFile.router,prefix="/candidates/files", tags=["Fichier de candidatures"])
+app.include_router(selectionFile.router,prefix="/candidates/selection", tags=["Candidatures sélectionnées"])
+app.include_router(feature.router,prefix="/candidates", tags=["Caractéristiques des candidatures"])
+app.include_router(intConstraint.router,prefix="/candidates/constraints", tags=["Contraintes sur des entiers"])
+app.include_router(enumConstraint.router,prefix="/candidates/constraints", tags=["Contraintes sur des énumérations"])
