c5e96a848f506a3e756e17887234f5a2c34ce7ca
iamklaus <iamklaus01@gmail.com>
2022-07-28 16:40:14 +0100

    Configs parameters changed

--- database.py
+++ database.py
@@ -1,11 +1,9 @@
 import databases
 import sqlalchemy
 
-DATABASE_URL = "postgresql://klaus:sc_api#2022@localhost/selectd"
+DATABASE_URL = "postgresql://klaus:sc_api_2022@localhost/selectd"
 
 database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
 
-engine = sqlalchemy.create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = sqlalchemy.create_engine(DATABASE_URL)
