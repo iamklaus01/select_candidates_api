f3ca9f6d1ea8c273e54d5bc56579b76b4f7f5896
iamklaus <iamklaus01@gmail.com>
2022-08-30 22:42:04 +0100

    Feat : Cors middleware added - some new endpoints added

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import database
 from routers import auth, candidatesFile, selectionFile, feature, intConstraint, enumConstraint
 
 app = FastAPI(title="Celect")
+
+origins = [
+    "http://localhost:5500",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
@@ -13,7 +26,7 @@ async def startup():
 async def shutdown():
     await database.disconnect()
 
-app.include_router(auth.router,prefix="/admin", tags=["Administration"])
+app.include_router(auth.router,prefix="/auth", tags=["Administration"])
 app.include_router(candidatesFile.router,prefix="/candidates/files", tags=["Fichier de candidatures"])
 app.include_router(selectionFile.router,prefix="/candidates/selection", tags=["Candidatures sélectionnées"])
 app.include_router(feature.router,prefix="/candidates", tags=["Caractéristiques des candidatures"])
