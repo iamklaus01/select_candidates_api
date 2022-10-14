945cd3ffe4388fc39647694a648e92b90485e46c
iamklaus <iamklaus01@gmail.com>
2022-10-14 18:13:50 +0100

    Style : Formatting routes tag name

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database import database
-from routers import auth, candidatesFile, selectionFile, feature, intConstraint, enumConstraint
+from routers import admin, auth, candidatesFile, selectionFile, feature, intConstraint, enumConstraint
 
 app = FastAPI(title="Celect")
 
@@ -26,9 +26,10 @@ async def startup():
 async def shutdown():
     await database.disconnect()
 
-app.include_router(auth.router,prefix="/auth", tags=["Administration"])
+app.include_router(auth.router,prefix="/auth", tags=["Authentification"])
 app.include_router(candidatesFile.router,prefix="/candidates/files", tags=["Fichier de candidatures"])
 app.include_router(selectionFile.router,prefix="/candidates/selection", tags=["Candidatures sélectionnées"])
 app.include_router(feature.router,prefix="/candidates", tags=["Caractéristiques des candidatures"])
 app.include_router(intConstraint.router,prefix="/candidates/constraints", tags=["Contraintes sur des entiers"])
 app.include_router(enumConstraint.router,prefix="/candidates/constraints", tags=["Contraintes sur des énumérations"])
+app.include_router(admin.router, prefix="/admin", tags=["Administration"])
