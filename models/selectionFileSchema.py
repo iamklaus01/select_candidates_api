06c05c1c8c2431e53bc732797e93737d19035dbf
iamklaus <iamklaus01@gmail.com>
2022-10-14 18:11:24 +0100

    Feat : stats column added to selectionFile model

--- models/selectionFileSchema.py
+++ models/selectionFileSchema.py
@@ -3,9 +3,15 @@ from pydantic import BaseModel
 
 class SelectionFileIn(BaseModel):
     base64File : str
+    status : str
+    n_sol : int
+    satisfaction : int
     candidatesFile_id : int
 
 class SelectionFile(BaseModel):
     id : int
     base64File : str
+    status : str
+    n_sol : int
+    satisfaction : int
     candidatesFile_id : int
