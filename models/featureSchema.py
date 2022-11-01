18b55641de7d34fda45ec95d6f6d09197c08c0e6
iamklaus <iamklaus01@gmail.com>
2022-11-01 23:58:45 +0100

    Feat : New pydantic schema for saving features

--- models/featureSchema.py
+++ models/featureSchema.py
@@ -1,5 +1,5 @@
+from typing import List
 from pydantic import BaseModel
-
 from tables import ValueType
 
 class FeatureIn(BaseModel):
@@ -13,3 +13,11 @@ class Feature(BaseModel):
     label : str
     valueType : ValueType
     candidatesFile_id : int
+
+class FeatureSaveIn(BaseModel):
+    name : str
+    type : str
+
+class AllFeatureIn(BaseModel):
+    features : List[FeatureSaveIn]
+    c_file_id : int
