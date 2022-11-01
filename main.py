d8fc6d071d2f966a71e21fbd7a68d4eb906b5334
iamklaus <iamklaus01@gmail.com>
2022-11-01 23:54:58 +0100

    Fix : CORS middleware allow_origins modified

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["http://127.0.0.1:5500"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
