3b62c8e7e4363128dfcb9c39127a47be1fc5e7c8
iamklaus <iamklaus01@gmail.com>
2022-11-04 09:45:38 +0100

    Feat : Return list of solutions

--- solver/solver.py
+++ solver/solver.py
@@ -27,16 +27,20 @@ async def solve(c_file : CandidateFile, int_features : List[Feature], enum_featu
 
     # Filtering datas with integer contraints define on integer types features
     datas_filtered = datas.copy(deep=True)
+    has_to_solve = False
+
     for feature in int_features:
         int_constraint = await get_constraint(feature.id, ValueType.integer)
-        datas_filtered = datas_filtered[ datas_filtered[feature.label] >= int_constraint.min_value ]
-        datas_filtered = datas_filtered[ datas_filtered[feature.label] <= int_constraint.max_value ]
+        if int_constraint:
+            has_to_solve = True
+            datas_filtered = datas_filtered[ datas_filtered[feature.label] >= int_constraint.min_value ]
+            datas_filtered = datas_filtered[ datas_filtered[feature.label] <= int_constraint.max_value ]
 
     datas_filtered.reset_index(inplace=True, drop=True)
 
     # For debugging
-    print("Liste de candidats après filtrage")
-    print(datas_filtered)
+    # print("Liste de candidats après filtrage")
+    # print(datas_filtered)
 
     id_col_name = list(datas_filtered.columns)[0]
     all_id = datas_filtered[id_col_name].tolist()
@@ -59,34 +63,44 @@ async def solve(c_file : CandidateFile, int_features : List[Feature], enum_featu
 
         # For each feature value on which constraints has been addded, add this constraint
         enum_c = await get_constraint(feature.id, ValueType.multiple)
-        for single_enum_c in enum_c:
-            corresponding_int = repeated_values.index(single_enum_c.value) + 1
-            s = sum([ selected[i]*feature_all_values[i] for i in range(len(all_id)) if feature_all_values[i]== corresponding_int ])
+        if enum_c:
+            has_to_solve = True
+            for single_enum_c in enum_c:
+                corresponding_int = repeated_values.index(single_enum_c.value) + 1
+                s = sum([ selected[i]*feature_all_values[i] for i in range(len(all_id)) if feature_all_values[i]== corresponding_int ])
 
-            # Adding properly constraint depending of the metric
-            number = corresponding_int * single_enum_c.number
+                # Adding properly constraint depending of the metric
+                number = corresponding_int * single_enum_c.number
 
-            if(Metric[single_enum_c.metric] == Metric.lessThan):
-                model.Add(s <= number)
-            elif(Metric[single_enum_c.metric] == Metric.moreThan):
-                model.Add(s >= number)
-            else:
-                model.Add(s == number)
+                if(Metric[single_enum_c.metric] == Metric.lessThan):
+                    model.Add(s <= number)
+                elif(Metric[single_enum_c.metric] == Metric.moreThan):
+                    model.Add(s >= number)
+                else:
+                    model.Add(s == number)
 
     # Proceed to the resolution
-    solution_printer = SolutionFormat(datas_filtered, selected)
+    solution_printer = SolutionFormat(datas_filtered, selected, 10)
 
     #Enumerate all solutions.
     solver.parameters.enumerate_all_solutions = True
 
     # Solving properly
-    status = solver.Solve(model, solution_printer)
-    number_of_solutions = solution_printer.solution_count()
+    if(has_to_solve):
+        status = solver.Solve(model, solution_printer)
+        number_of_solutions = solution_printer.solution_count()
 
-    print('Status = %s' % solver.StatusName(status))
-    print('Number of solutions found: %i' % number_of_solutions)
+        print('Status = %s' % solver.StatusName(status))
+        print('Number of solutions found: %i' % number_of_solutions)
 
-    return {
-        'status' : solver.StatusName(status),
-        'number_of_solutions' : number_of_solutions
-    }
+        return {
+            'status' : solver.StatusName(status),
+            'number_of_solutions' : number_of_solutions,
+            'solutions' : solution_printer.get_all_solutions()
+        }
+    else:
+         return {
+            'status' : "NOTHING TO SOLVE",
+            'number_of_solutions' : 0,
+            'solutions' : []
+        }
