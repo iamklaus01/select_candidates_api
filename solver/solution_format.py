5989149cc2237632e1a30f635e9d53e7d84722ea
iamklaus <iamklaus01@gmail.com>
2022-11-04 09:45:10 +0100

    Feat : Stop research after a number of solutions found

--- solver/solution_format.py
+++ solver/solution_format.py
@@ -4,11 +4,13 @@ class SolutionFormat(cp_model.CpSolverSolutionCallback):
     """Print intermediate solutions."""
     
     
-    def __init__(self, datas, variables):
+    def __init__(self, datas, variables, limit:int):
         cp_model.CpSolverSolutionCallback.__init__(self)
         self.__data = datas
         self.__variables = variables
+        self.__all_solutions = []
         self.__solution_count = 0
+        self.__solution_limit = limit
 
     def on_solution_callback(self):
         one_list = []
@@ -17,9 +19,19 @@ class SolutionFormat(cp_model.CpSolverSolutionCallback):
             if(self.Value(v) == 1):
                 one_list.append(v.Index())
             #print('%s=%i' % (v, self.Value(v)), end=' ')
+        self.__all_solutions.append(self.__data.iloc[one_list])
+        if self.__solution_limit<0 and self.__solution_count >= self.__solution_limit:
+            print('Stop search after %i solutions' % self.__solution_limit)
+            self.StopSearch()
         # print("Liste n°%i de Candidats sélectionnés " % (self.__solution_count))
         # print(self.__data.iloc[one_list])
         # print()
 
     def solution_count(self):
         return self.__solution_count
+
+    def get_data(self):
+        return self.__data
+
+    def get_all_solutions(self):
+        return self.__all_solutions
