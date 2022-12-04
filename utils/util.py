a873d968453031f8ac7082bef4d973548ebb92c2
iamklaus <iamklaus01@gmail.com>
2022-12-04 18:59:32 +0100

    Feat : Methods for data formatting

--- utils/util.py
+++ utils/util.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from math import trunc
 from models.featureSchema import Feature
 
-from tables import users, candidates_files
+from tables import Metric, users, candidates_files
 from database import database
 
 
@@ -42,7 +42,7 @@ def extract_features(path:str):
 def get_features_and_type(columns, types):
     features_types = {}
     for col, type in zip(columns, types):
-        features_types[col] = ( "Enumeration", "Entier" ) [type == dtype('int64')]
+        features_types[col] = ( "Enumeration", "Number" ) [type in [ dtype('int64'), dtype('float64') ]]
 
     return features_types
 
@@ -82,3 +82,58 @@ async def get_details_on_enum_feature(feature:Feature):
             }
         )
     return {feature.label : value_with_count}
+
+def format_int_constraints(iconstraints, features):
+    all_i_constraints = []
+    for iconstraint in iconstraints:
+        all_i_constraints.append({
+            'feature_name' : features[iconstraint.feature_id],
+            'min' : iconstraint.min_value,
+            'max' : iconstraint.max_value,
+        })
+    return all_i_constraints
+
+def format_enum_constraints(econstraints, features):
+    all_e_constraints = []
+    for econstraint in econstraints:
+        all_e_constraints.append({
+            'feature_name' : features[econstraint.feature_id],
+            'value' : econstraint.value,
+            'metric' : get_metric(econstraint.metric, econstraint.number),
+        })
+    return all_e_constraints
+
+def format_user_files(c_files, sol_files):
+    all_files = []
+    for sol_file in sol_files:
+        all_files.append({
+            'extension' : c_files[sol_file.candidatesFile_id]['extension'],
+            'path': c_files[sol_file.candidatesFile_id]['path'],
+            'nbre_sol': sol_file.nbre_sol,
+            'status' : sol_file.status,
+            'sol_file_id' : sol_file.id,
+        })
+    return all_files
+
+def get_metric(m:Metric, number):
+    if(Metric[m] == Metric.equalTo):
+        return "Exactly"+" "*3+str(number)
+    elif(Metric[m] == Metric.lessThan):
+        return "Less Than"+" "*3+str(number)
+    else:
+        return "More Than"+" "*3+str(number)
+
+def format_feature_label_id(features):
+    f_id = {}
+    for f in features:
+        f_id[f.id] = f.label
+    return f_id
+
+def format_candidates_files_data(candidates_files):
+    id_attr = {}
+    for elmt in candidates_files:
+        id_attr[elmt.id] = {
+            'extension' : elmt.extension,
+            'path' : elmt.path
+        }
+    return id_attr
