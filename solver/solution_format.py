9fc3b74c07f0f9d3033fdfb4be5d5a4d7d5771f4
iamklaus <iamklaus01@gmail.com>
2022-12-04 18:57:57 +0100

    Solutions formatted to dict before sending it

--- solver/solution_format.py
+++ solver/solution_format.py
@@ -1,4 +1,6 @@
+import pandas as pd
 from ortools.sat.python import cp_model
+
 
 class SolutionFormat(cp_model.CpSolverSolutionCallback):
     """Print intermediate solutions."""
@@ -19,9 +21,9 @@ class SolutionFormat(cp_model.CpSolverSolutionCallback):
             if(self.Value(v) == 1):
                 one_list.append(v.Index())
             #print('%s=%i' % (v, self.Value(v)), end=' ')
-        self.__all_solutions.append(self.__data.iloc[one_list])
-        if self.__solution_limit<0 and self.__solution_count >= self.__solution_limit:
-            print('Stop search after %i solutions' % self.__solution_limit)
+        self.__all_solutions.append(pd.DataFrame(self.__data.iloc[one_list]))
+        if self.__solution_limit > 0 and self.__solution_count >= self.__solution_limit:
+            # print('Stop search after %i solutions' % self.__solution_limit)
             self.StopSearch()
         # print("Liste n°%i de Candidats sélectionnés " % (self.__solution_count))
         # print(self.__data.iloc[one_list])
@@ -34,4 +36,6 @@ class SolutionFormat(cp_model.CpSolverSolutionCallback):
         return self.__data
 
     def get_all_solutions(self):
+        for i in range(len(self.__all_solutions)):
+            self.__all_solutions[i] = self.__all_solutions[i].to_dict(orient = "records")
         return self.__all_solutions
