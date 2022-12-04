a8d1936ec6971449bff3e41afa5f0e9a22ef5a62
iamklaus <iamklaus01@gmail.com>
2022-12-04 18:53:58 +0100

    Fixed : ValueType integer changed to ValueType numerous

--- routers/candidatesFile.py
+++ routers/candidatesFile.py
@@ -1,18 +1,21 @@
 from operator import and_
-from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, Form, status
+from fastapi import APIRouter, Depends, HTTPException, UploadFile, File, status
 from os import getcwd, remove
 from typing import List
+from sqlalchemy import select
+
 from database import database
 import shutil
 
 
-from tables import Role, ValueType, candidates_files, features, users
+from tables import Role, ValueType, candidates_files, features, users, integer_constraints, enum_constraints
 from token_dependencie import JWTBearer
 from solver.solver import solve
 from models.candidatesFileSchema import CandidateFile
-from utils.util import check_user, rename_file, get_file_path, extract_features, FileType
+from models.featureSchema import AllFeatureIn
+from utils.util import check_user, rename_file, get_file_path, extract_features, FileType, format_int_constraints, format_enum_constraints, format_feature_label_id
 
-FILE_NOT_FOUND_MESSAGE = "Fichier non retrouvé! Rassurez-vous d'avoir le bon identifiant"
+FILE_NOT_FOUND_MESSAGE = "File not found! Make sure you have the correct ID"
 
 router = APIRouter()
 
@@ -22,9 +25,9 @@ async def get_candidates_files(user_id :int):
     query = users.select().where(users.c.id  == user_id)
     user = await database.fetch_one(query)
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Utilisateur introuvable")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     if Role[user.role] != Role.admin:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Vous n'êtes pas autorisé(e)s pour une telle opération")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="You are not authorized for such an operation")
     query = candidates_files.select()
     all_files = await database.fetch_all(query)
     return all_files
@@ -33,7 +36,7 @@ async def get_candidates_files(user_id :int):
 async def get_users_candidates_files(user_id:int):
 
     if not await check_user(user_id):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Utilisateur non retrouvé")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
     query = candidates_files.select().where(candidates_files.c.user_id == user_id)
     user_files = await database.fetch_all(query)
@@ -49,36 +52,64 @@ async def get_single_candidate_file(id:int):
 
     return {**candidate_file}
 
+@router.get("/get_constraints/{c_file_id}", status_code=status.HTTP_200_OK, dependencies=[Depends(JWTBearer())])
+async def get_all_constraints(c_file_id : int):
+    query = features.select().where(features.c.candidatesFile_id == c_file_id)
+    all_features = await database.fetch_all(query)
+    int_features_id = []
+    enum_features_id = []
+    for feature in all_features:
+        if(ValueType[feature.valueType] == ValueType.number):
+            int_features_id.append(feature.id)
+        else:
+            enum_features_id.append(feature.id)
+    query = select(integer_constraints).where(integer_constraints.c.feature_id.in_(int_features_id)).join(features, integer_constraints.c.feature_id == features.c.id)
+    iconstraints = await database.fetch_all(query)   
 
-@router.post("/add/{user_id}", response_model=CandidateFile, status_code=status.HTTP_201_CREATED, dependencies=[Depends(JWTBearer())])
-async def store_candidates_files(user_id:int ,c_file:UploadFile = File(...), ignore_col:int = Form(...)):
+    query = select(enum_constraints).where(enum_constraints.c.feature_id.in_(enum_features_id)).join(features, enum_constraints.c.feature_id == features.c.id)
+    econstraints = await database.fetch_all(query)   
+    f_id = format_feature_label_id(all_features)
+    return{
+        'iconstraints' : format_int_constraints(iconstraints, f_id),
+        'econstraints' : format_enum_constraints(econstraints, f_id)
+    }
+
+@router.post("/add/{user_id}", status_code=status.HTTP_201_CREATED, dependencies=[Depends(JWTBearer())])
+async def store_candidates_files(user_id:int ,c_file:UploadFile = File(...)):
     if not await check_user(user_id):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Utilisateur introuvable. Vous devriez avoir un compte pour utiliser nos services.")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found. You should have an account to use our services.")
 
     c_file.filename = rename_file(c_file.filename)
     with open(f'files/candidates/{c_file.filename}', "wb") as buffer:
         shutil.copyfileobj(c_file.file, buffer)
-    file_path = get_file_path(FileType.cFile, c_file.filename)
-    c_file_features = extract_features(file_path, ignore_col)
 
+    file_path = get_file_path(FileType.cFile, c_file.filename)
     query = candidates_files.insert().values(
-        extension = c_file.filename.split(".")[1],
-        path = file_path,
-        user_id = user_id
+    extension = c_file.filename.split(".")[1],
+    path = file_path,
+    user_id = user_id
     )
     record_id = await database.execute(query)
+    if(record_id):
+        c_file_features = extract_features(file_path)
+        return {"c_file_id" : record_id, "data": c_file_features}
 
-    for i in range(len(c_file_features[0])):
+    return {"c_file_id" : -1, "data": None}
+
+@router.post("/add_features/{user_id}", status_code=status.HTTP_201_CREATED, dependencies=[Depends(JWTBearer())])
+async def store_candidates_file_feature(user_id:int , data:AllFeatureIn):
+    if not await check_user(user_id):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found. You should have an account to use our services.")
+    for feature in data.features:
         query = features.insert().values(
-            label = c_file_features[0][i],
-            valueType = ( ValueType.multiple, ValueType.integer ) [c_file_features[1][i] == "int64"],
-            candidatesFile_id = record_id
+            label = feature.name,
+            valueType = ( ValueType.multiple, ValueType.number ) [feature.type == "Number"],
+            candidatesFile_id = data.c_file_id
         )
         await database.execute(query)
 
-    query = candidates_files.select().where(candidates_files.c.id == record_id)
-    row = await database.fetch_one(query)
-    return {**row}
+    
+    return {"message" : "Features have been saved successfully"}
 
 
 @router.delete("/remove", status_code = status.HTTP_200_OK, dependencies=[Depends(JWTBearer())])
@@ -104,18 +135,23 @@ async def delete_candidates_file(id:int):
         }
 
 
-
-@router.get("/solve/{file_id}", dependencies=[Depends(JWTBearer())])
-async def resolve(file_id : int):
+@router.get("/solve/{file_id}/{limit}", dependencies=[Depends(JWTBearer())])
+async def resolve(file_id : int, limit: int):
     query = candidates_files.select().where(candidates_files.c.id == file_id)
     candidate_file = await database.fetch_one(query)
     if not candidate_file:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=FILE_NOT_FOUND_MESSAGE)
 
-    query = features.select().where(and_(features.c.candidatesFile_id  == file_id, features.c.valueType == ValueType.integer))
+    query = features.select().where(and_(features.c.candidatesFile_id  == file_id, features.c.valueType == ValueType.number))
     all_int_features = await database.fetch_all(query)
 
     query = features.select().where(and_(features.c.candidatesFile_id  == file_id, features.c.valueType == ValueType.multiple))
     all_enum_features = await database.fetch_all(query)
 
-    return await solve(candidate_file, all_int_features, all_enum_features)
+
+    return await solve(candidate_file, all_int_features, all_enum_features, limit)
+
+
+# from starlette.responses import FileResponse
+
+# return FileResponse(file_location, media_type='application/octet-stream',filename=file_name)
--- routers/feature.py
+++ routers/feature.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from operator import and_
+from fastapi import APIRouter, Depends, HTTPException, status, Form
 from typing import List
 from database import database
 
@@ -6,34 +7,57 @@ from database import database
 from models.featureSchema import Feature
 from tables import ValueType, features
 from token_dependencie import JWTBearer
-from utils.util import add_int_constraint, add_enum_constraint
+from utils.util import get_details_on_int_feature, get_details_on_enum_feature
 
 
 router = APIRouter()
 
-@router.get("/features", response_model=List[Feature], dependencies=[Depends(JWTBearer())])
-async def get_file_features(cd_file_id :int):
-    query = features.select().where(features.c.candidatesFile_id  == cd_file_id)
-    all_features = await database.fetch_all(query)
-    return all_features
+
+@router.get("/features/int/{cd_file_id}", response_model=List[Feature], dependencies=[Depends(JWTBearer())])
+async def get_file_enum_features(cd_file_id :int):
+    query = features.select().where(and_(features.c.candidatesFile_id  == cd_file_id, features.c.valueType == ValueType.number))
+    all_int_features = await database.fetch_all(query)
+    return all_int_features
+
+@router.get("/features/enum/{cd_file_id}", response_model=List[Feature], dependencies=[Depends(JWTBearer())])
+async def get_file_enum_features(cd_file_id :int):
+    query = features.select().where(and_(features.c.candidatesFile_id  == cd_file_id, features.c.valueType == ValueType.multiple))
+    all_enum_features = await database.fetch_all(query)
+    return all_enum_features
 
 @router.get("/features/single", response_model=Feature, dependencies=[Depends(JWTBearer())])
 async def get_single_file_feature(id:int):
     query = features.select().where(features.c.id == id)
     single_feature = await database.fetch_one(query)
     if not single_feature:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Caractéristique introuvable ou non prise en compte.")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Feature not found or not taken into account.")
     return single_feature
 
-@router.get("/features/add/constraints", dependencies=[Depends(JWTBearer())])
-async def get_details_on_feature_values(feature_id:int):
-    query = features.select().where(features.c.id == feature_id)
-    single_feature = await database.fetch_one(query)
-    if not single_feature:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Caractéristique introuvable ou non prise en compte.")
 
-    if ValueType[single_feature.valueType] == ValueType.integer:
-        return await add_int_constraint(single_feature)
-    else:
-        return await add_enum_constraint(single_feature)
+@router.get("/features/int/details/{cd_file_id}", dependencies=[Depends(JWTBearer())])
+async def get_details_int_features(cd_file_id :int):
+    query = features.select().where(and_(features.c.candidatesFile_id  == cd_file_id, features.c.valueType == ValueType.number))
+    all_int_features = await database.fetch_all(query)
 
+    if not all_int_features:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Features not found or not taken into account.")
+
+    details = []
+    for feature in all_int_features:
+        details.append(await get_details_on_int_feature(feature))
+    
+    return details
+
+@router.get("/features/enum/details/{cd_file_id}", dependencies=[Depends(JWTBearer())])
+async def get_details_enum_features(cd_file_id :int):
+    query = features.select().where(and_(features.c.candidatesFile_id  == cd_file_id, features.c.valueType == ValueType.multiple))
+    all_int_features = await database.fetch_all(query)
+
+    if not all_int_features:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Features not found or not taken into account.")
+
+    details = []
+    for feature in all_int_features:
+        details.append(await get_details_on_enum_feature(feature))
+    
+    return details
