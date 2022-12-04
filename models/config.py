4dd24a6b24bee86428e38bd1ee81bde79744503f
iamklaus <iamklaus01@gmail.com>
2022-12-04 18:37:39 +0100

    Feat : Config file for mail sending

---
+++ models/config.py
@@ -0,0 +1,16 @@
+from pydantic import BaseSettings, EmailStr
+
+
+class Settings(BaseSettings):
+
+    EMAIL_HOST : str
+    EMAIL_PORT : int
+    EMAIL_USERNAME : str
+    EMAIL_PASSWORD : str
+    EMAIL_FROM : EmailStr
+
+    class Config:
+        env_file = './.env'
+
+
+settings = Settings()
