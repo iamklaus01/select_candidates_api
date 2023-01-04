388744d4357a71f2e87d68fd061386a872f1caf4
iamklaus <iamklaus01@gmail.com>
2023-01-04 09:46:59 +0100

    Feat : Pydantic model added for numericalConstraint to take into account optimization

--- models/integerConstraintSchema.py
+++ models/integerConstraintSchema.py
@@ -1,17 +1,31 @@
 from typing import List
 from pydantic import BaseModel
 
+from tables import OptimizeType
+
 
 class IntegerConstraintIn(BaseModel):
     min_value : int
     max_value : int
     feature_id : int
 
+class OptIntegerConstraintIn(BaseModel):
+    min_value : int
+    max_value : int
+    feature_id : int
+    coefficient : float
+    optimize : OptimizeType
+
 class IntegerConstraint(BaseModel):
     id : int
     min_value : int
     max_value : int
     feature_id : int
+    coefficient : float
+    optimize : OptimizeType
 
 class AllIntegerConstraintIn(BaseModel):
     data : List[IntegerConstraintIn]
+
+class AllOptIntegerConstraintIn(BaseModel):
+    data : List[OptIntegerConstraintIn]
